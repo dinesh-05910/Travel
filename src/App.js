c97bf81b4116b8ef507552664044f9cab9434af0
Chinta Dinesh <dineshchinta2001@gmail.com>
2022-11-25 00:03:39 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import './App.css';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/Services' element={<Services />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
